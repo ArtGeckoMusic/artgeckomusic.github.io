25930a0ad58254190d022380f45213c5bdfc37db
Aaron Willette <awillette98@gmail.com>
2024-06-30 19:06:43 -0700

    began work on new website

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// const Xebra = require("./xebra");
-
 $(document).ready(function() {
-  let webSocket = new WebSocket("ws://127.0.0.1");
-  // var xebraState;
-  // var options = {
-  //   hostname: "127.0.0.1",
-  //   port: 80,
-  //   supported_objects: ["button"]
-  // };
-  // xebraState = new Xebra.State(options);
+  
 });
