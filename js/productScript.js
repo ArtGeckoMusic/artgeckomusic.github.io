5efed7e0355bb8d2e02875612d4119577e02a32b
Aaron Willette <awillette98@gmail.com>
2024-07-07 21:25:06 -0700

    file reorg, work on merch product page

--- js/productScript.js
+++ js/productScript.js
@@ -1,7 +1,12 @@
 $(document).ready(() => {
+  
+});
+
+
+function showOrHideProduct() {
   const urlParams = new URLSearchParams(window.location.search);
   const productValue = urlParams.get('product');
   console.log(productValue);  
 
-  $('.' + productValue).css('display', 'none');
-});
+  $('#' + productValue).css('display', 'block');
+}
