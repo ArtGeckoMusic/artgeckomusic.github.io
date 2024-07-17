38963f3e1ac17975b4a55efdaa412445eeb76cf7
Aaron Willette <awillette98@gmail.com>
2024-07-16 17:15:58 -0700

    merch pic bug fixes

--- js/productScript.js
+++ js/productScript.js
@@ -1,4 +1,19 @@
 $(document).ready(() => {
-  console.log('initing splide');
-  new Splide('.splide').mount();  
+  // console.log('initing beanie splide');
+  // new Splide('#beanieSlideshow').mount();
+  // console.log('initing shirt1 splide');
+  // new Splide('#shirt1Slideshow').mount();
+  // console.log('initing shirt2 splide');
+  // new Splide('#shirt2Slideshow').mount();
+  
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('initing beanie splide');
+  new Splide('#beanieSlideshow').mount();
+  console.log('initing shirt1 splide');
+  new Splide('#shirt1Slideshow').mount();
+  console.log('initing shirt2 splide');
+  new Splide('#shirt2Slideshow').mount();
 });
